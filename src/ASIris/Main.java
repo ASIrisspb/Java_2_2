6d316eebc0086048acf94da679de00a4bd87dac9
ALEXANDR IVANOV <39660147+ASIrisspb@users.noreply.github.com>
2020-08-28 03:31:13 +0300

    1. Напишите метод, на вход которого подаётся двумерный строковый массив размером 4х4, при подаче массива другого размера необходимо бросить исключение MyArraySizeException. 2. Далее метод должен пройтись по всем элементам массива, преобразовать в int, и просуммировать. Если в каком-то элементе массива преобразование не удалось (например, в ячейке лежит символ или текст вместо числа), должно быть брошено исключение MyArrayDataException, с детализацией в какой именно ячейке лежат неверные данные. 3. В методе main() вызвать полученный метод, обработать возможные исключения MySizeArrayException и MyArrayDataException, и вывести результат расчета.

--- src/ASIris/Main.java
+++ src/ASIris/Main.java
@@ -1,32 +1,84 @@
 package ASIris;
 
+import javafx.util.Pair;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	int[] numbers = new int[] {0,1,2,3,4,5};
-	try {
-        calc(numbers);
-    } catch (ArithmeticException e) {
-        System.out.println("Division by zero");
+
+        //массив для обработки
+        String[][] strings = new String[][]
+                {   {"1", "2", "3", "4в"},
+                    {"1", "2", "3в", "4"},
+                    {"1", "2", "3", "4"},
+                    {"1", "2", "3", "4"}   };
+
+        //рабочий метод
+        myArrayMethod(strings);
+
     }
 
-        System.out.println("Program is normal end");
-    }
-
-    private static float calc(int[] numbers) {
-        float result = 1;
-        for (int i = numbers.length-1; i >= 0 ; i--) {
-            result *= numbers[i];
-            System.out.println(result);
+    //метод принимающий на вход массив
+    private static void myArrayMethod(String[][] myArray) {
+        //проверяем размерность переданного массива
+        try {
+            if (myArray.length != 4) {
+                throw new MyArraySizeException();
+            } else {
+                for (String[] strings : myArray) {
+                    if (strings.length != 4) {
+                        throw new MyArraySizeException();
+                    }
+                }
+            }
+            //если исключение MyArraySizeException не возникло, то идем дальше
+            //переменная для хранения суммы
+            int sum = 0;
+            //переменная для хранения результата перевода из строки в число
+            int numberOfSting = 0;
+            //проходим по всем элементам массива
+            for (int i = 0; i < myArray.length; i++) {
+                for (int j = 0; j < myArray[i].length; j++) {
+                    //обнуляем, чтобы не получилось ложной суммы
+                    numberOfSting = 0;
+                    try {
+                        numberOfSting = Integer.parseInt(myArray[i][j]);
+                    } catch (NumberFormatException e) {
+                        Pair<Integer, Integer> indexOfMistake = new Pair<>(i,j);
+                        MyArrayDataException.indexOfMistake.add(indexOfMistake);
+                    }
+                    sum += numberOfSting;
+                }
+            }
+            if (MyArrayDataException.indexOfMistake.size() != 0) {
+                MyArrayDataException.printMistake();
+            }
+            System.out.println("Сумма распознанных элементов массива: " + sum);
+        //если размерность неправильная, то выводим сообщение
+        } catch (MyArraySizeException e) {
+            e.printMistake();
         }
-        return result;
+
     }
 }
-
-
-class ArrayWithZeroException extends Exception {
-
-    public ArrayWithZeroException(String message) {
-        super(message);
+//класс обнаружения ошибки размерности массива
+    class MyArraySizeException extends Exception {
+        public void printMistake() {
+            System.out.println("Передан массив неправильного размера!");
+        }
     }
-}
+
+//класс обнаружения и фиксации ошибки "нечислового" элемента массива
+    class MyArrayDataException extends Exception {
+        static ArrayList<Pair<Integer,Integer>> indexOfMistake = new ArrayList<>();
+
+        static public void printMistake() {
+            System.out.println("В массиве обнаружены ошибки (вместо чисел стоят недопустимые символы) " +
+                    "- их индексы приведены ниже:");
+            System.out.println(MyArrayDataException.indexOfMistake);
+        }
+    }
+
+
+
